fef013eb83c62d89486f22af44d474e22c28216a
Bjørnar Borge <72221999+bjurneiz@users.noreply.github.com>
2022-02-09 15:32:24 +0100

    Fixed padding on self (main window)

--- sudoku.py
+++ sudoku.py
@@ -1,16 +1,15 @@
 import tkinter as tk
-from tkinter import Button, ttk
+from tkinter import ttk
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-
         # Configure the root windows
         self.title('Suduko')
-
+        self.configure(padx=20)
         # Labels
         overskrift = ttk.Label(self, text='Sudoku', font=('Helvetica', 36))
-        overskrift.grid(row=0, columnspan=9, padx=5, pady=5)
+        overskrift.grid(row=0, columnspan=10, padx=5, pady=20)
 
         # Crate the Sudoku boards
         entries = []
@@ -30,21 +29,21 @@ class App(tk.Tk):
                 if (col+1) % 3 == 0 and (col+1) < grid_dim: # skip for last column
                     pad_x = (0, 10)
 
-                entry.grid(row=1+row, column=col, ipadx=10, ipady=10, padx=pad_x, pady=pad_y)
+                entry.grid(row=1+row, column=1+col, ipadx=10, ipady=10, padx=pad_x, pady=pad_y)
         entries.append(entry)
 
         # Buttons on the bottom of the screen
         # Reset the board and start over
         reset = ttk.Button(self, text='Reset', command=self.reset_sudoku)
-        reset.grid(row=10, column=0, columnspan=9, sticky='W', padx=5, pady=5)
+        reset.grid(row=10, column=0, columnspan=10, sticky='W', padx=5, pady=20)
 
         # Generate new sudoku
         generate = ttk.Button(self, text='Generate', command=self.generate_sudoku)
-        generate.grid(row=10, column=0, columnspan=9, padx=5, pady=5)
+        generate.grid(row=10, column=0, columnspan=10, padx=5, pady=20)
 
         # Finish
         finish = ttk.Button(self, text='Finish', command=self.finish_sudoku)
-        finish.grid(row=10, column=0, columnspan=9, sticky='E', padx=5, pady=5)
+        finish.grid(row=10, column=0, columnspan=10, sticky='E', padx=5, pady=20)
 
 
     def reset_sudoku(self):
@@ -55,8 +54,6 @@ class App(tk.Tk):
 
     def finish_sudoku(self):
         print('finish_sudoku')
-
-    
 
 if __name__ == '__main__':
     App().mainloop()
